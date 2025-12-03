4ba1b1560a72249bb4d2f8a2cae3f11726809dd8
israel2206 <brenoisrael931@gmail.com>
2025-12-03 12:53:16 -0300

    personalizando a página inicial, o casdastro do candidato e personalizando o resto

---
+++ projeto/htmls/configuracoes/script.js
@@ -0,0 +1,20 @@
+const pessoas = {
+    1 : {
+        nome:"Márcio",
+        cargo:"Desenvolvedor Web",
+        idade: 30,
+        cidade:"Veredinha",
+        habilidades: ["HTML", "CSS", "JavaScript"],
+        descricao: "Com 5 anos de experiência em desenvolvimento web."
+    },
+
+    2 : {
+        nome:"Robespierre",
+        cargo:"Marketing Digital",
+        idade:23,
+        cidade: "Maxashuseti",
+        habilidades:"todas",
+        descricao: "Especialista em Marketing Digital. Estratégico e analítico, impulsiona campanhas que geram resultados."
+    }
+};
+
